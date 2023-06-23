1cc767fca2797d55e41058dc5578aa310be97129
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2023-06-23 13:37:47 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const AppWrapper = styled.div``;
+
+function App() {
+  return (
+    <AppWrapper>
+      <h1>Hello, World!</h1>
+    </AppWrapper>
+  );
+}
+
+export default App;
